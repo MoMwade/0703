cfdf6e527bc4ca2fb85f8b13dbf5866ad25b3b86
2891692713@qq.com <xiaowang2222>
2023-12-12 16:15:10 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,12 @@
     <h1>{{ msg }}</h1>
     <p>
       张总，唱首歌吧，别摸了
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">王家轩怎么还没开始,就结束了,那女人刚躺下你就软了</a>.
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">张宝其三厘米</a>.
     </p>
     <h3>张宝其是王佳轩的爹(王家轩)</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">短小无力</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">年幼不举</a></li>
     </ul>
     <h3>张宝其暴力大飞王佳炫了(王家轩)</h3>
     <ul>
